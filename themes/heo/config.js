b71ff2670034a2c76bfa0990b44ed2bfdbbd131f
Luoye <arvinwang007@gmail.com>
2025-08-13 17:40:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到叶落的博客', url: 'https://blog.yeluo.io' },
-    { title: '分享出海跨境和AI', url: 'https://https://blog.yeluo.io' }
+    { title: '分享出海跨境和AI', url: 'https://blog.yeluo.io' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -28,13 +28,13 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
@@ -118,9 +118,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://blog.yeluo.io',
+  HEO_SOCIAL_CARD_TITLE_2: '加我的微信交流分享',
+  HEO_SOCIAL_CARD_TITLE_3: '点击加我的微信',
+  HEO_SOCIAL_CARD_URL: 'https://blog.yeluo.io/about',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
